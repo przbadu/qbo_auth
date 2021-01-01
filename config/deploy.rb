51494325d955719592c25b65e3281bfc9acca519
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2021-01-01 17:46:08 +0545

    update capistrano to take current branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "qbo_auth"
 set :repo_url, "git@github.com:przbadu/qbo_auth.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deploy/#{fetch :application}"
