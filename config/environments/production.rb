fa84e0146e7952171235ac89fd989d0f531d9f2e
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2021-01-17 09:53:17 +0545

    Fixing actioncable configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,10 +32,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.mount_path = '/cable'
+  config.action_cable.url = 'wss://qboapi.tk/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = ['https://qboapi.tk', 'https://qbosync.netlify.app/']
+  config.action_cable.allowed_request_origins = ['https://qboapi.tk', 'https://qbosync.netlify.app']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
